33882abff31de3d3bddf6651bddee18e62dbcc2a
Spenser Black <spenserblack01@gmail.com>
2024-10-07 10:58:24 -0400

    Use `reg.exe` for system theme detection
    Should be faster than spawning a `powershell.exe` process.

--- lua/colorscheme/systemtheme/windows.lua
+++ lua/colorscheme/systemtheme/windows.lua
@@ -1,24 +1,20 @@
--- TODO Spawning a PowerShell process seems to be really slow. Find a better way to read
---      the registry?
 local function get_system_theme()
   local system_root = os.getenv("SystemRoot") or ""
   -- NOTE If %SystemRoot% isn't set for whatever reason, escape
   if system_root == "" then
     return "light"
   end
-  local powershell_path = table.concat(
+  local reg_path = table.concat(
     {
       system_root,
       "System32",
-      "WindowsPowerShell",
-      "v1.0",
-      "powershell.exe",
+      "reg.exe",
     },
     "\\"
   )
   local registry_property_path = table.concat(
     {
-      "HKCU:",
+      "HKCU",
       "SOFTWARE",
       "Microsoft",
       "Windows",
@@ -28,19 +24,18 @@ local function get_system_theme()
     },
     "\\"
   )
-  local ps_command = string.format(
-    "Get-ItemPropertyValue -Path %s  -Name AppsUseLightTheme",
+  local command = string.format(
+    "%s QUERY %s /v AppsUseLightTheme",
+    reg_path,
     registry_property_path
   )
-  local command = string.format(
-    "%s -NonInteractive -Command %q",
-    powershell_path,
-    ps_command
-  )
   local handle = io.popen(command)
-  local apps_use_light_theme = handle:read("*a"):match("^%s*(%d+)%s*$")
+  local apps_use_light_theme = handle:read("*a"):match("0x(%x+)")
   handle:close()
-  if apps_use_light_theme == "0" then
+  if not apps_use_light_theme then
+    return "light"
+  end
+  if tonumber(apps_use_light_theme, 16) == 0 then
     return "dark"
   end
   return "light"
